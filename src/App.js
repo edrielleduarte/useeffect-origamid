d71c1bcaff0d00a14eef99bc47b83e36266abcd5
Edrielle Duarte <edrielleduarte@gmail.com>
2022-08-24 00:42:16 -0300

    Exercício 01 - useEffect - Origamid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Produto from "./Produto";
 
 function App() {
+  const [produto, setProduto] = useState(null)
+  useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto')
+    if(produtoLocal !== null){
+      setProduto(produtoLocal)
+    } 
+  }, [])
+
+  useEffect(() => {
+    if(produto !== null){
+      window.localStorage.setItem('produto', produto)
+    }
+    
+  }, [produto])
+
+  const handClick = ({ target }) => {
+    setProduto(target.innerText)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <div>
+        <h1>Preferência: {produto}</h1>
+        <button onClick={handClick}  style={{ marginRight: '1rem' }}>Notebook</button>
+        <button onClick={handClick}> Smartphone</button>
+        <Produto produto={produto} />
+    </div>
     </div>
   );
 }
